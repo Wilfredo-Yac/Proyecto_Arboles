8f25a4efdca7c50f714057962ae6e1ad44522604
Yotam <wilfredoyotam@gmail.com>
2026-05-08 19:11:49 -0600

    Feature: Implementacion del algoritmo de eliminacion cubriendo los 3 casos fundamentales

--- arbol_bst.py
+++ arbol_bst.py
@@ -15,9 +15,7 @@ class ArbolBST:
         elif valor > nodo_actual.valor:
             nodo_actual.derecho = self._insertar_recursivo(nodo_actual.derecho, valor)
         return nodo_actual
-    # ==========================================
     #            RECORRIDOS RECURSIVOS
-    # ==========================================
     def obtener_preorden(self):
         resultado = []
         self._preorden_recursivo(self.raiz, resultado)
@@ -50,10 +48,7 @@ class ArbolBST:
             self._postorden_recursivo(nodo_actual.izquierdo, resultado)  # Izquierda
             self._postorden_recursivo(nodo_actual.derecho, resultado)  # Derecha
             resultado.append(nodo_actual.valor)  # Raíz
-
-    # ==========================================
     #             BÚSQUEDA RECURSIVA
-    # ==========================================
     def buscar(self, valor):
         camino = []
         encontrado = self._buscar_recursivo(self.raiz, valor, camino)
@@ -70,3 +65,40 @@ class ArbolBST:
         if valor < nodo_actual.valor:
             return self._buscar_recursivo(nodo_actual.izquierdo, valor, camino)
         return self._buscar_recursivo(nodo_actual.derecho, valor, camino)
+
+    #            ELIMINACIÓN RECURSIVA
+    def eliminar(self, valor):
+        self.raiz = self._eliminar_recursivo(self.raiz, valor)
+
+    def _eliminar_recursivo(self, nodo_actual, valor):
+        if nodo_actual is None:
+            return nodo_actual
+
+        # 1. Buscar el nodo a eliminar
+        if valor < nodo_actual.valor:
+            nodo_actual.izquierdo = self._eliminar_recursivo(nodo_actual.izquierdo, valor)
+        elif valor > nodo_actual.valor:
+            nodo_actual.derecho = self._eliminar_recursivo(nodo_actual.derecho, valor)
+        else:
+
+            # CASO 1 y 2: Nodo hoja o con un solo hijo
+            if nodo_actual.izquierdo is None:
+                return nodo_actual.derecho
+            elif nodo_actual.derecho is None:
+                return nodo_actual.izquierdo
+
+            # CASO 3: Nodo con dos hijos
+            # Buscamos el sucesor en inorden (el menor del subárbol derecho)
+            sucesor = self._encontrar_minimo(nodo_actual.derecho)
+            # Reemplazamos el valor por el del sucesor
+            nodo_actual.valor = sucesor.valor
+            # Eliminamos recursivamente el sucesor en el subárbol derecho
+            nodo_actual.derecho = self._eliminar_recursivo(nodo_actual.derecho, sucesor.valor)
+
+        return nodo_actual
+
+    def _encontrar_minimo(self, nodo):
+        nodo_actual = nodo
+        while nodo_actual.izquierdo is not None:
+            nodo_actual = nodo_actual.izquierdo
+        return nodo_actual
